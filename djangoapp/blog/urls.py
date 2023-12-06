bf6c607a3c955aa1e7ab67496b8b1ec621d7c854
Henrique Allan <henriqueallan16@gmail.com>
2023-12-06 16:42:01 -0300

    Otimizando a área do post com diversos detalhes

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -7,7 +7,9 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('page/', views.page, name='page'),
+    path('page/<slug:slug>/', views.page, name='page'),
     path('post/<slug:slug>/', views.post, name='post'),
+    path('created_by/<int:author_pk>/', views.created_by, name='created_by'),
+    path('category/<slug:slug>/', views.category, name='category'),
     path('', views.index, name='index'),
 ]
