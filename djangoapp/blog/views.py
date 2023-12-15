f0d144d04285dedaf7a17e9de48e27bc5ce7f361
Henrique Allan <henriqueallan16@gmail.com>
2023-12-15 17:40:46 -0300

    Passando a Function Based View 'created_by' para Class Based View

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,18 +1,17 @@
 from typing import Any
 from django.core.paginator import Paginator
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.http import Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.views.generic import ListView
 
 
 PER_PAGE = 9
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
     ordering = '-pk',
@@ -25,31 +24,8 @@ class PostListView(ListView):
         context.update({
             'page_title': 'Home - ',
         })
-        
-        for c, i in context.items():
-            print(f'{" CHAVE ":=^50}')
-            print(c)
-            print(f'{" Item ":-^50}')
-            print(i, end='\n\n\n\n\n\n\n')
 
         return context
-
-
-# def index(request):
-#     posts = Post.objects.get_published()
-
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home - ',
-#         }
-#     )
 
 
 def category(request, slug):
@@ -120,6 +96,47 @@ def search(request):
     )
 
 
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._temp_context = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        user = self._temp_context['user']
+
+        user_full_name = user.username
+        page_title = f'Posts de {user_full_name} - '
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+
+        ctx.update({
+            'page_title': page_title,
+        })
+        return ctx
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
+
+
 def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first()
 
@@ -128,7 +145,7 @@ def created_by(request, author_pk):
 
     user_full_name = user.username
 
-    if (user.first_name):
+    if user.first_name:
         user_full_name = f'{user.first_name} {user.last_name}'
 
     page_title = f'Posts de {user_full_name} - '
